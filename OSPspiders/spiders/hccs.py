d501c6c422770c68e51696942977e89a72ad1d75
Jonathan Lesser <jel2189@columbia.edu>
2016-11-08 01:27:55 -0500

    HCCS spider functioning correctly

--- OSPspiders/spiders/hccs.py
+++ OSPspiders/spiders/hccs.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import syllascrape
 
 class HccsSpider(scrapy.Spider):
-    name = "hccs"
-    allowed_domains = ["http://learning.hccs.edu/resources_search_results.html"]
-    start_urls = (
-        'http://learning.hccs.edu/resources_search_results.html/',
-    )
+	name = "hccs"
+	allowed_domains = ["learning.hccs.edu","http://learning.hccs.edu/resources_search_results.html"]
+	start_urls = (
+			'http://learning.hccs.edu/resources_search_results.html/',
+		     )
 
-    def parse(self, response):
-        return scrapy.FormRequest.from_response(
-		response,
-		formname = 'search',
-		formdata = {'checkbox':'Animation',}
-	)
+#All posted syllabi on HCCS database have "syllabus" in document title, so search for "syllabus"
+	def parse(self, response):
+		return scrapy.FormRequest(
+				response.url,
+				formdata = {'SearchableText':'Syllabus'},
+				method = 'POST',
+				callback = self.parseResults,
+				)
+
+	def parseResults(self, response):
+		print(response.css('html').extract())
+		return

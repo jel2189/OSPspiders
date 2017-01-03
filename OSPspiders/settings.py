6cc9ee69a0b9858a58e4fff9c31a0ee7aaba640d
Jonathan Lesser <jel2189@columbia.edu>
2017-01-03 11:35:24 -0500

    hccs.py functioning

--- OSPspiders/settings.py
+++ OSPspiders/settings.py
@@ -20,8 +20,8 @@ EDITOR = 'vim'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'OSPspiders (+http://www.yourdomain.com)'
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# Obey robots.txt rules, or don't?
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
